6d18a8dffa8f960aa49fc336fd47dcbb0d6f6f22
Jimmy <jimmy.wallener@protonmail.com>
2022-11-13 19:30:22 +0100

    added file not found page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import AddMovie from './pages/AddMovie';
 import Login from './pages/Login';
 import Movie from './pages/Movie';
 import Movies from './pages/Movies';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/movies' element={<Movies />} />
           <Route path='/movies/:title' element={<Movie />} />
           <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
           <Route
             path='/addMovie'
             element={
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className='bg-gray-900 min-h-screen w-screen flex justify-center items-center'>
+      <div
+        className={`col-span-4 p-2 my-2 w-[25vw] h-[25vh] flex flex-col justify-center items-center shadow rounded-2xl bg-white`}
+      >
+        <div>
+          <p className='font-bold text-center text-xl'>404 - Page Not Found!</p>
+          <p className='block p-4 text-center font-light'>
+            Oh no! The page you are looking for does not exist!
+          </p>
+        </div>
+        <div>
+          <button
+            className='bg-red-500 hover:bg-red-700 mt-6 text-white font-bold py-2 px-4 rounded'
+            onClick={() => navigate('/')}
+          >
+            Take me back...!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
